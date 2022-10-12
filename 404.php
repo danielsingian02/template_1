c6d20db1b3b3fa1408a0677d52f5c21f69e00fb2
Daniel Singian <daniel.singian02@gmail.com>
2022-10-12 14:53:13 +0800

    Added styling to the 404 page

--- 404.php
+++ 404.php
@@ -1,30 +1,34 @@
 <?php
-get_header(); ?>
-  <div class="site-content-404">
-	  <article class="no-results">
+get_header();
+?>
 
-	    <header class="entry-header">
-	      <h1 class="page-title"><?php esc_html_e( ' Page Not Found :( '); ?></h1>
-	    </header><!-- .entry-header -->
+<div class="site-content-404">
+	<article class="no-results">
+		<!-- Header -->
+		<header class="entry-header">
+		  <h1 class="page-title"><?php esc_html_e( ' Page Not Found :( ' ); ?></h1>
+		</header>
 
-	    <div class="entry-content">
-			<p><?php esc_html_e( 'It looks like nothing was found at this location.'); ?></p>
-	    </div><!-- .entry-content -->
+		<!-- Header Content -->
+		<div class="entry-content">
+			<p><?php esc_html_e( 'It looks like nothing was found at this location.' ); ?></p>
+		</div>
 
-        <div class="searchbar-404">
-    <form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-<div>
-<input class="text-404" type="text" value=" " name="s" id="s" placeholder="Search..."/>
-<input type="submit" class="submit button-404" name="submit" value="<?php _e('Search');?>" />
+		<div class="searchbar-404">
+			<form method="get" id="searchform" action="<?php bloginfo( 'url' ); ?>">
+				<div class="search-container">
+					<input class="text-404" type="text" value="" name="s" id="s" placeholder="Search..."/>
+					<input type="submit" class="submit button-404" name="submit" value="<?php _e( 'Search' ); ?>" />
+				</div>
+			</form>
+		</div>
+		
+		<div class="home-link">
+			<p class="p-404"><a class="home-link" href="<?php echo home_url( '/' ); ?>"><i class="fa fa-arrow-left" aria-hidden="true"></i> Go back</a></p>
+		</div>
+
+	</article>
 </div>
-</form>
-        </div>
-        <div class="home-link">
-			<p class="404p"><a class="home_link" href="<?php echo home_url('/'); ?>"> <--- Go back</a></p>
-	    </div>
-
-	  </article><!-- .no-results -->
-	</div><!-- .site-content -->
-    <?php
+<?php
 get_footer();
 ?>
